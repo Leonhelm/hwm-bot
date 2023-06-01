c6d383739739e89efa6587d23d99a338d8fd944d
Demkin Vitalii <vitalii.demkin@factory5.ai>
2023-06-01 10:16:53 +0300

    wip

--- bot.js
+++ bot.js
@@ -2,7 +2,7 @@ import { applyForJob } from './commands/applyForJob.js';
 import { hunt } from './commands/hunt.js';
 
 const runBot = async () => {
-    const hours = new Date().getHours();
+    const hours = new Date().getUTCHours() + 3; // Делаем таймзону как на сервере игры
 
     if (hours < 9 || hours > 22) {
         console.log(`Не запускаем бота потому что час: ${hours}`);
